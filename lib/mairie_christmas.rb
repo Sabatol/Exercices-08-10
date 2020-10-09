a752c3123e394a0531a50857416a7c3eea18c687
Sabatol <71655588+Sabatol@users.noreply.github.com>
2020-10-09 09:13:20 +0200

    Update mairie_christmas.rb

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -5,11 +5,14 @@ require 'open-uri'
 @city_list = Array.new 
 @city_mail = Array.new 
 
+
+#Méthode pour obtenir la liste des villes du val d'oise à partir d'un url (open via Nokogiri) et les placer dans city_list
 def get_city_list
   page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
   return citys = page.xpath(('//a[@class="lientxt"]')).each{|city| @city_list << city.text.downcase.gsub(" ","-")}
 end
 
+#Méthode pour obtenir la liste des emails des villes du val d'oise à partir d'un url (open via Nokogiri), d'un chemin à parcourir... pour enfin les placer dans city_mail
 def get_city_email
   @city_list.each do |city|
   page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/95/#{city}.html"))
@@ -18,6 +21,7 @@ def get_city_email
   end
 end
 
+#On fusionne les arrays et on met en forme le hash !
 def fusion
   hash_final = Hash[*@city_list.zip(@city_mail).flatten]
   return puts hash_final.each_slice(1).map(&:to_h)
